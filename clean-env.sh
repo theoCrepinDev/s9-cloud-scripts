dc8de23df0c659123a2bf3a54c07dd9de247d85e
Théo CREPIN <theo.crepin@efrei.net>
2024-01-28 13:10:30 +0100

    add clean mongo resources

--- clean-env.sh
+++ clean-env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo Cleaning up
 kubectl delete -f postgres.yaml
+kubectl delete -f mongo.yaml
 kubectl delete -f bdiscount-items-back.yaml
 kubectl delete -f bdiscount-authentication-back.yaml
 kubectl delete -f gateway.yaml
